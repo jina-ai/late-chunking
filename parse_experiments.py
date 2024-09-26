f3eb17d7b11eb3e145c091ee888dc8eee050fbf0
dannyjameswilliams <danny@weaviate.io>
2024-09-26 13:42:09 +0100

    experiment for WikimQA

--- parse_experiments.py
+++ parse_experiments.py
@@ -12,18 +12,12 @@ import os
 
 
 
-
-
-
-
-
-
 if __name__ == "__main__":
 
     task_name = "LEMBWikimQARetrievalChunked"
     soft_boundary_dir = "results-soft-boundary"
     hard_boundary_dir = "results-hard-boundary"
-    truncate_dir = "results-truncation"
+    truncated_boundary_dir = "results-truncation"
 
     embed_sizes = [1024, 2048, 4096, 8192]
     overlap_sizes = [32, 64, 128, 256, 512]
@@ -52,34 +46,56 @@ if __name__ == "__main__":
         hard_boundary_ndcgs[embed_i] = results["scores"]["test"][0]["ndcg_at_10"]
         hard_boundary_maps[embed_i]  = results["scores"]["test"][0]["map_at_10"]
 
+    truncation_dir = os.path.join(truncated_boundary_dir, "no_model_name_available", "no_revision_available", f"{task_name}.json")
+    with open(truncation_dir, "r") as f:
+        results = json.load(f)
+    
+    truncated_boundary_ndcg = results["scores"]["test"][0]["ndcg_at_10"]
+    truncated_boundary_map  = results["scores"]["test"][0]["map_at_10"]
 
     # NDCG
-    fig, ax = plt.subplots(len(embed_sizes), 1, figsize=(12, 9))
+    plt.style.use('seaborn-v0_8-whitegrid')
+    plt.rcParams.update({'font.size': 14, 'font.family': 'serif'})
 
-    for embed_i, embed_size in enumerate(embed_sizes):
+    fig, ax = plt.subplots(1, 1, figsize=(12, 9), sharex=True)
 
-        ax[embed_i].plot(overlap_sizes, soft_boundary_ndcgs[embed_i], label="Soft Boundary")
-        ax[embed_i].scatter(overlap_sizes, soft_boundary_ndcgs[embed_i])
-        ax[embed_i].axhline(hard_boundary_ndcgs[embed_i], color="red", linestyle="--", label="Hard Boundary")
+    colors = plt.cm.get_cmap('plasma', len(embed_sizes))(np.linspace(0, 1, len(embed_sizes), endpoint=False))
+    colors = ["red", "blue", "green", "orange"]
 
-        ax[embed_i].set_title(f"Embed Size: {embed_size}")
-        ax[embed_i].set_xlabel("Overlap Size")
-        ax[embed_i].set_ylabel("nCDG@10")
+    for embed_i, embed_size in enumerate(embed_sizes[:-1]):
 
-    ax[embed_i].legend()
-    fig.tight_layout()
+        ax.plot([o - embed_i*5 for o in overlap_sizes], soft_boundary_ndcgs[embed_i], label=f"Soft Boundary ({embed_size})", color=colors[embed_i], marker='o', linestyle='-', linewidth=2, markersize=6)
+        ax.axhline(hard_boundary_ndcgs[embed_i], linestyle="--", label=f"Hard Boundary ({embed_size})", linewidth=2, color=colors[embed_i])
+
+        ax.set_title(f"Embed Size: {embed_size}", fontsize=16)
+        ax.set_ylabel("nCDG@10", fontsize=14)
+        ax.legend(loc='best', fontsize=12)
+
+    ax.axhline(truncated_boundary_ndcg, label=f"Truncated Boundary (at 8192)", color="k", linestyle='-.', linewidth=2)
+    ax.set_xlabel("Overlap Size", fontsize=14)
+    fig.suptitle("NDCG@10 for Different Embed Sizes and Overlap Sizes", fontsize=18)
+    fig.tight_layout(rect=[0, 0, 1, 0.96])
 
     # MAP
-    fig, ax = plt.subplots(len(embed_sizes), 1, figsize=(12, 9))
-    for embed_i, embed_size in enumerate(embed_sizes):
+    plt.style.use('seaborn-v0_8-whitegrid')
+    plt.rcParams.update({'font.size': 14, 'font.family': 'serif'})
 
-        ax[embed_i].plot(overlap_sizes, soft_boundary_maps[embed_i], label="Soft Boundary")
-        ax[embed_i].scatter(overlap_sizes, soft_boundary_maps[embed_i])
-        ax[embed_i].axhline(hard_boundary_maps[embed_i], color="red", linestyle="--", label="Hard Boundary")
+    fig, ax = plt.subplots(1, 1, figsize=(12, 9), sharex=True)
 
-        ax[embed_i].set_title(f"Embed Size: {embed_size}")
-        ax[embed_i].set_xlabel("Overlap Size")
-        ax[embed_i].set_ylabel("mAP@10")
+    colors = plt.cm.get_cmap('plasma', len(embed_sizes))(np.linspace(0, 1, len(embed_sizes), endpoint=False))
+    colors = ["red", "blue", "green", "orange"]
 
-    ax[embed_i].legend()
-    fig.tight_layout()
+    for embed_i, embed_size in enumerate(embed_sizes[:-1]):
+
+        ax.plot([o - embed_i*5 for o in overlap_sizes], soft_boundary_maps[embed_i], label=f"Soft Boundary ({embed_size})", color=colors[embed_i], marker='o', linestyle='-', linewidth=2, markersize=6)
+        ax.axhline(hard_boundary_maps[embed_i], linestyle="--", label=f"Hard Boundary ({embed_size})", linewidth=2, color=colors[embed_i])
+
+        ax.set_title(f"Embed Size: {embed_size}", fontsize=16)
+        ax.set_ylabel("mAP@10", fontsize=14)
+        ax.legend(loc='best', fontsize=12)
+
+
+    ax.axhline(truncated_boundary_map, label=f"Truncated Boundary (at 8192)", color="k", linestyle='-.', linewidth=2)
+    ax.set_xlabel("Overlap Size", fontsize=14)
+    fig.suptitle("mAP@10 for Different Embed Sizes and Overlap Sizes", fontsize=18)
+    fig.tight_layout(rect=[0, 0, 1, 0.96])
