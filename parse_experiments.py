57cc8bac94ccc59f7e5cfb1c2fe1083816d511ea
dannyjameswilliams <fs19144@bristol.ac.uk>
2024-09-26 16:37:32 +0100

    more tasks for soft/hard

--- parse_experiments.py
+++ parse_experiments.py
@@ -19,60 +19,74 @@ if __name__ == "__main__":
     hard_boundary_dir = "results-hard-boundary"
     truncated_boundary_dir = "results-truncation"
 
+    # == Load results
+
     embed_sizes = [1024, 2048, 4096, 8192]
     overlap_sizes = [32, 64, 128, 256, 512]
+    task_names = ["QMSum", "SummScreenFD", "WikimQA"]
 
-    soft_boundary_ndcgs = np.empty((len(embed_sizes), len(overlap_sizes)))
-    hard_boundary_ndcgs = np.empty(len(embed_sizes))
+    soft_boundary_ndcgs = np.empty((len(task_names), len(embed_sizes), len(overlap_sizes)))
+    hard_boundary_ndcgs = np.empty((len(task_names), len(embed_sizes)))
 
-    soft_boundary_maps = np.empty((len(embed_sizes), len(overlap_sizes)))
-    hard_boundary_maps = np.empty(len(embed_sizes))
+    soft_boundary_maps = np.empty((len(task_names), len(embed_sizes), len(overlap_sizes)))
+    hard_boundary_maps = np.empty((len(task_names), len(embed_sizes)))
 
-    for embed_i, embed_size in enumerate(embed_sizes):
+    for task_i, task_name in enumerate(task_names):
 
-        for overlap_i, overlap_size in enumerate(overlap_sizes):
-            
-            soft_dir = os.path.join(soft_boundary_dir, f"embed_size_{embed_size}", f"overlap_{overlap_size}", "no_model_name_available", "no_revision_available", f"{task_name}.json")
+        task_name = f"LEMB{task_name}RetrievalChunked"
+
+        for embed_i, embed_size in enumerate(embed_sizes):
+
+            for overlap_i, overlap_size in enumerate(overlap_sizes):
+                
+                soft_dir = os.path.join(soft_boundary_dir, f"embed_size_{embed_size}", f"overlap_{overlap_size}", "no_model_name_available", "no_revision_available", f"{task_name}.json")
+                with open(soft_dir, "r") as f:
+                    results = json.load(f)
+
+                soft_boundary_ndcgs[task_i, embed_i, overlap_i] = results["scores"]["test"][0]["ndcg_at_10"]
+                soft_boundary_maps[task_i, embed_i, overlap_i]  = results["scores"]["test"][0]["map_at_10"]
+
+            soft_dir = os.path.join(hard_boundary_dir, f"embed_size_{embed_size}", "no_model_name_available", "no_revision_available", f"{task_name}.json")
             with open(soft_dir, "r") as f:
                 results = json.load(f)
+            
+            hard_boundary_ndcgs[task_i, embed_i] = results["scores"]["test"][0]["ndcg_at_10"]
+            hard_boundary_maps[task_i, embed_i]  = results["scores"]["test"][0]["map_at_10"]
 
-            soft_boundary_ndcgs[embed_i, overlap_i] = results["scores"]["test"][0]["ndcg_at_10"]
-            soft_boundary_maps[embed_i, overlap_i]  = results["scores"]["test"][0]["map_at_10"]
-
-        soft_dir = os.path.join(hard_boundary_dir, f"embed_size_{embed_size}", "no_model_name_available", "no_revision_available", f"{task_name}.json")
-        with open(soft_dir, "r") as f:
-            results = json.load(f)
-        
-        hard_boundary_ndcgs[embed_i] = results["scores"]["test"][0]["ndcg_at_10"]
-        hard_boundary_maps[embed_i]  = results["scores"]["test"][0]["map_at_10"]
-
-    truncation_dir = os.path.join(truncated_boundary_dir, "no_model_name_available", "no_revision_available", f"{task_name}.json")
-    with open(truncation_dir, "r") as f:
-        results = json.load(f)
+    # truncation_dir = os.path.join(truncated_boundary_dir, "no_model_name_available", "no_revision_available", f"{task_name}.json")
+    # with open(truncation_dir, "r") as f:
+    #     results = json.load(f)
     
-    truncated_boundary_ndcg = results["scores"]["test"][0]["ndcg_at_10"]
-    truncated_boundary_map  = results["scores"]["test"][0]["map_at_10"]
+    # truncated_boundary_ndcg = results["scores"]["test"][0]["ndcg_at_10"]
+    # truncated_boundary_map  = results["scores"]["test"][0]["map_at_10"]
+
+
+    # == Plot (by task separately)
 
     # NDCG
     plt.style.use('seaborn-v0_8-whitegrid')
     plt.rcParams.update({'font.size': 14, 'font.family': 'serif'})
 
-    fig, ax = plt.subplots(1, 1, figsize=(12, 9), sharex=True)
+    fig, ax = plt.subplots(len(task_names), 1, figsize=(12, 3*len(task_names)), sharex=True)
 
-    colors = plt.cm.get_cmap('plasma', len(embed_sizes))(np.linspace(0, 1, len(embed_sizes), endpoint=False))
+    # colors = plt.cm.get_cmap('plasma', len(embed_sizes))(np.linspace(0, 1, len(embed_sizes), endpoint=False))
     colors = ["red", "blue", "green", "orange"]
 
-    for embed_i, embed_size in enumerate(embed_sizes[:-1]):
+    for task_i in range(len(task_names)):
+        for embed_i, embed_size in enumerate(embed_sizes[:-1]):
 
-        ax.plot([o - embed_i*5 for o in overlap_sizes], soft_boundary_ndcgs[embed_i], label=f"Soft Boundary ({embed_size})", color=colors[embed_i], marker='o', linestyle='-', linewidth=2, markersize=6)
-        ax.axhline(hard_boundary_ndcgs[embed_i], linestyle="--", label=f"Hard Boundary ({embed_size})", linewidth=2, color=colors[embed_i])
+            ax[task_i].plot([o - embed_i*5 for o in overlap_sizes], soft_boundary_ndcgs[task_i, embed_i], label=f"Soft Boundary ({embed_size})", color=colors[embed_i], marker='o', linestyle='-', linewidth=2, markersize=6)
+            ax[task_i].axhline(hard_boundary_ndcgs[task_i, embed_i], linestyle="--", label=f"Hard Boundary ({embed_size})", linewidth=2, color=colors[embed_i])
 
-        ax.set_title(f"Embed Size: {embed_size}", fontsize=16)
-        ax.set_ylabel("nCDG@10", fontsize=14)
-        ax.legend(loc='best', fontsize=12)
+            ax[task_i].set_title(f"Embed Size: {embed_size}", fontsize=16)
+            ax[task_i].set_ylabel("nCDG@10", fontsize=14)
+            ax[task_i].legend(loc='best', fontsize=12)
+        
+        ax[task_i].set_title("Task: " + task_names[task_i], fontsize=16)
 
-    ax.axhline(truncated_boundary_ndcg, label=f"Truncated Boundary (at 8192)", color="k", linestyle='-.', linewidth=2)
-    ax.set_xlabel("Overlap Size", fontsize=14)
+    # ax.axhline(truncated_boundary_ndcg, label=f"Truncated Boundary (at 8192)", color="k", linestyle='-.', linewidth=2)
+    ax[task_i].set_xlabel("Overlap Size", fontsize=14)
+
     fig.suptitle("NDCG@10 for Different Embed Sizes and Overlap Sizes", fontsize=18)
     fig.tight_layout(rect=[0, 0, 1, 0.96])
 
