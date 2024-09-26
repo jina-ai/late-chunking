3f226a5550274538ca6a314719351ac012faa74a
dannyjameswilliams <danny@weaviate.io>
2024-09-26 14:10:53 +0100

    added remainder of longembed datasets

--- chunked_pooling/chunked_eval_tasks.py
+++ chunked_pooling/chunked_eval_tasks.py
@@ -296,6 +296,7 @@ class LEMBWikimQARetrievalChunked(AbsTaskChunkedRetrieval):
 
         self.data_loaded = True
 
+
 class LEMBNarrativeQARetrievalChunked(AbsTaskChunkedRetrieval):
     """
     modified from https://github.com/embeddings-benchmark/mteb/blob/main/mteb/tasks/Retrieval/eng/LEMBWikimQARetrieval.py
@@ -307,7 +308,7 @@ class LEMBNarrativeQARetrievalChunked(AbsTaskChunkedRetrieval):
         name="LEMBNarrativeQARetrievalChunked",
         dataset={
             "path": "dwzhu/LongEmbed",
-            "revision": "6e346642246bfb4928c560ee08640dc84d074e8c",
+            "revision": "10039a580487dacecf79db69166e17ace3ede392",
             "name": "LEMBNarrativeQARetrieval",
         },
         reference="https://huggingface.co/datasets/dwzhu/LongEmbed",
@@ -343,8 +344,8 @@ class LEMBNarrativeQARetrievalChunked(AbsTaskChunkedRetrieval):
             "n_samples": {_EVAL_SPLIT: 500},
             "avg_character_length": {
                 "test": {
-                    "average_document_length": 326753.5323943662,
-                    "average_query_length": 47.89453536223562,
+                    "average_document_length": 326753.53,
+                    "average_query_length": 47.89,
                     "num_documents": 300,
                     "num_queries": 300,
                     "average_relevant_docs_per_query": 1.0,
@@ -374,6 +375,166 @@ class LEMBNarrativeQARetrievalChunked(AbsTaskChunkedRetrieval):
         self.relevant_docs = {self._EVAL_SPLIT: qrels}
 
         self.data_loaded = True
+
+class LEMBSummScreenFDRetrievalChunked(AbsTaskChunkedRetrieval):
+    """
+    modified from https://github.com/embeddings-benchmark/mteb/blob/main/mteb/tasks/Retrieval/eng/LEMBWikimQARetrieval.py
+    """
+
+    _EVAL_SPLIT = "test"
+
+    metadata = TaskMetadata(
+        name="LEMBSummScreenFDRetrievalChunked",
+        dataset={
+            "path": "dwzhu/LongEmbed",
+            "revision": "10039a580487dacecf79db69166e17ace3ede392",
+            "name": "LEMBSummScreenFDRetrieval",
+        },
+        reference="https://huggingface.co/datasets/dwzhu/LongEmbed",
+        description=("summ_screen_fd subset of dwzhu/LongEmbed dataset."),
+        type="Retrieval",
+        category="s2p",
+        modalities=["text"],
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["eng-Latn"],
+        main_score="ndcg_at_10",
+        date=("1950-01-01", "2019-12-31"),
+        domains=None,
+        socioeconomic_status=None,
+        n_samples=None,
+        avg_character_length=None,
+        form=None,
+        text_creation=None,
+        task_subtypes=["Article retrieval"],
+        license="not specified",
+        annotations_creators="derived",
+        dialect=[],
+        sample_creation="found",
+        bibtex_citation="""
+            @inproceedings{ho2020constructing,
+                title={Constructing A Multi-hop QA Dataset for Comprehensive Evaluation of Reasoning Steps},
+                author={Ho, Xanh and Nguyen, Anh-Khoa Duong and Sugawara, Saku and Aizawa, Akiko},
+                booktitle={Proceedings of the 28th International Conference on Computational Linguistics},
+                pages={6609--6625},
+                year={2020}
+            }
+        """,
+        descriptive_stats={
+            "n_samples": {_EVAL_SPLIT: 500},
+            "avg_character_length": {
+                "test": {
+                    "average_document_length": 30854.327,
+                    "average_query_length": 591.49,
+                    "num_documents": 300,
+                    "num_queries": 300,
+                    "average_relevant_docs_per_query": 1.0,
+                }
+            },
+        },
+    )
+
+    def load_data(self, **kwargs):
+        if self.data_loaded:
+            return
+
+        dataset_dict = {**self.metadata.dataset}
+        dataset_dict['name'] = 'summ_screen_fd'
+
+        query_list = datasets.load_dataset(**dataset_dict)["queries"]
+        queries = {row["qid"]: row["text"] for row in query_list}
+
+        corpus_list = datasets.load_dataset(**dataset_dict)["corpus"]
+        corpus = {row["doc_id"]: {"text": row["text"]} for row in corpus_list}
+
+        qrels_list = datasets.load_dataset(**dataset_dict)["qrels"]
+        qrels = {row["qid"]: {row["doc_id"]: 1} for row in qrels_list}
+
+        self.corpus = {self._EVAL_SPLIT: corpus}
+        self.queries = {self._EVAL_SPLIT: queries}
+        self.relevant_docs = {self._EVAL_SPLIT: qrels}
+
+        self.data_loaded = True
+
+
+class LEMBQMSumRetrievalRetrievalChunked(AbsTaskChunkedRetrieval):
+    """
+    modified from https://github.com/embeddings-benchmark/mteb/blob/main/mteb/tasks/Retrieval/eng/LEMBWikimQARetrieval.py
+    """
+
+    _EVAL_SPLIT = "test"
+
+    metadata = TaskMetadata(
+        name="LEMBQMSumRetrievalRetrievalChunked",
+        dataset={
+            "path": "dwzhu/LongEmbed",
+            "revision": "10039a580487dacecf79db69166e17ace3ede392",
+            "name": "LEMBQMSumRetrievalRetrieval",
+        },
+        reference="https://huggingface.co/datasets/dwzhu/LongEmbed",
+        description=("qmsum subset of dwzhu/LongEmbed dataset."),
+        type="Retrieval",
+        category="s2p",
+        modalities=["text"],
+        eval_splits=[_EVAL_SPLIT],
+        eval_langs=["eng-Latn"],
+        main_score="ndcg_at_10",
+        date=("1950-01-01", "2019-12-31"),
+        domains=None,
+        socioeconomic_status=None,
+        n_samples=None,
+        avg_character_length=None,
+        form=None,
+        text_creation=None,
+        task_subtypes=["Article retrieval"],
+        license="not specified",
+        annotations_creators="derived",
+        dialect=[],
+        sample_creation="found",
+        bibtex_citation="""
+            @inproceedings{ho2020constructing,
+                title={Constructing A Multi-hop QA Dataset for Comprehensive Evaluation of Reasoning Steps},
+                author={Ho, Xanh and Nguyen, Anh-Khoa Duong and Sugawara, Saku and Aizawa, Akiko},
+                booktitle={Proceedings of the 28th International Conference on Computational Linguistics},
+                pages={6609--6625},
+                year={2020}
+            }
+        """,
+        descriptive_stats={
+            "n_samples": {_EVAL_SPLIT: 500},
+            "avg_character_length": {
+                "test": {
+                    "average_document_length": 53335.817,
+                    "average_query_length": 433.50,
+                    "num_documents": 300,
+                    "num_queries": 300,
+                    "average_relevant_docs_per_query": 1.0,
+                }
+            },
+        },
+    )
+
+    def load_data(self, **kwargs):
+        if self.data_loaded:
+            return
+
+        dataset_dict = {**self.metadata.dataset}
+        dataset_dict['name'] = 'qmsum'
+
+        query_list = datasets.load_dataset(**dataset_dict)["queries"]
+        queries = {row["qid"]: row["text"] for row in query_list}
+
+        corpus_list = datasets.load_dataset(**dataset_dict)["corpus"]
+        corpus = {row["doc_id"]: {"text": row["text"]} for row in corpus_list}
+
+        qrels_list = datasets.load_dataset(**dataset_dict)["qrels"]
+        qrels = {row["qid"]: {row["doc_id"]: 1} for row in qrels_list}
+
+        self.corpus = {self._EVAL_SPLIT: corpus}
+        self.queries = {self._EVAL_SPLIT: queries}
+        self.relevant_docs = {self._EVAL_SPLIT: qrels}
+
+        self.data_loaded = True
+
 
 class LEMBNeedleRetrievalChunked(AbsTaskChunkedRetrieval):
     """
